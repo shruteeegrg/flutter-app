a0a97464206398a4eb98313e9e429495d5af21ad
Shruti Gurung <gurungshrutee44@gmail.com>
2025-09-21 16:33:22 +0545

    dashbaord

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:my_flutter/login.dart';
+import 'package:my_flutter/loginpage.dart';
+import 'package:my_flutter/signin.dart';
+import 'package:flutter/material.dart';
+import 'package:my_flutter/dashboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PCPS DEMO',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  dashboard(),
+    );
+  }
+}
+
+
+
